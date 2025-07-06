b44c0011ec09e74dce29792b42fde23379db634e
Amit Finkelstein <amittein@gmail.com>
2025-07-06 19:05:53 +0000

    Code style

--- custom_components/retry/__init__.py
+++ custom_components/retry/__init__.py
@@ -313,10 +313,7 @@ class RetryAction:
         self._attempt = 1
         self._retry_id = params.retry_data.get(ATTR_RETRY_ID)
         if ATTR_RETRY_ID not in params.retry_data:
-            if self._entity_id:
-                self._retry_id = self._entity_id
-            else:
-                self._retry_id = self._action
+            self._retry_id = self._entity_id or self._action
         self._str_cache = None
         self._start_id()
 
